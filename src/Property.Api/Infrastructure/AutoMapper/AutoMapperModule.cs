1c3ecf1e388b2b9320aa32ad0197f38ae65541c1
blakebyron <blake.byron@me.com>
2020-08-04 21:24:51 +0100

    Add AutoMapper and configure
    Add Automapper libraries, configure object mapping and implement in handlers

---
+++ src/Property.Api/Infrastructure/AutoMapper/AutoMapperModule.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using af = Autofac.Module;
+using AutoMapper;
+using Autofac;
+
+namespace Property.Api.Infrastructure.AutoMapper
+{
+    public class AutoMapperModule:af
+    {
+        private readonly Assembly assembly;
+
+        public AutoMapperModule(Assembly assembly)
+        {
+            this.assembly = assembly;
+        }
+
+        protected override void Load(ContainerBuilder builder)
+        {
+
+            var profiles =
+                from t in assembly.GetTypes()
+                where typeof(Profile).IsAssignableFrom(t)
+                select (Profile)Activator.CreateInstance(t);
+
+
+            var config = new MapperConfiguration(config =>
+            {
+                foreach (var profile in profiles)
+                {
+                    config.AddProfile(profile);
+                }
+
+            });
+
+            builder.RegisterInstance(config).As<MapperConfiguration>();
+
+            base.Load(builder);
+        }
+    }
+}
