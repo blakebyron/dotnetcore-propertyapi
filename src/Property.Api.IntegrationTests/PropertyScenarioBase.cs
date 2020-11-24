4386f44d48e446ab5c242d5fad10a311e3dc2372
blakebyron <blake.byron@me.com>
2020-11-24 22:08:45 +0000

    Tidy commented out code

--- src/Property.Api.IntegrationTests/PropertyScenarioBase.cs
+++ src/Property.Api.IntegrationTests/PropertyScenarioBase.cs
@@ -13,32 +13,6 @@ namespace Property.Api.IntegrationTests
     {
         public TestServer CreateServer()
         {
-            //var path = Assembly.GetAssembly(typeof(PropertyScenarioBase))
-            //    .Location;
-
-            //var hostBuilder = new WebHostBuilder()
-            //    .UseContentRoot(Path.GetDirectoryName(path))
-            //    .ConfigureAppConfiguration(cb =>
-            //    {
-            //        cb.AddJsonFile("appsettings.json", optional: false)
-            //        .AddEnvironmentVariables();
-            //    }).UseStartup<OrderingTestsStartup>();
-
-            //var testServer = new TestServer(hostBuilder);
-
-            //testServer.Host
-            //    .MigrateDbContext<OrderingContext>((context, services) =>
-            //    {
-            //        var env = services.GetService<IWebHostEnvironment>();
-            //        var settings = services.GetService<IOptions<OrderingSettings>>();
-            //        var logger = services.GetService<ILogger<OrderingContextSeed>>();
-
-            //        new OrderingContextSeed()
-            //            .SeedAsync(context, env, settings, logger)
-            //            .Wait();
-            //    })
-            //    .MigrateDbContext<IntegrationEventLogContext>((_, __) => { });
-
             var server = new TestServer(new WebHostBuilder()
             .ConfigureServices(service => service.AddAutofac())
             .ConfigureAppConfiguration(cd =>
@@ -49,22 +23,6 @@ namespace Property.Api.IntegrationTests
             .UseStartup<Startup>());
 
             return server;
-        }
-
-        public static class Get
-        {
-            public static string Orders = "api/v1/orders";
-
-            public static string OrderBy(int id)
-            {
-                return $"api/v1/orders/{id}";
-            }
-        }
-
-        public static class Put
-        {
-            public static string CancelOrder = "api/v1/orders/cancel";
-            public static string ShipOrder = "api/v1/orders/ship";
         }
     }
 }
